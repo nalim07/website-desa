0840c69bdfe34192c74ceaa0e0a55700abc5ab64
nalim07 <alimdulah@gmail.com>
2025-05-04 21:48:12 +0700

    Enhance website layout and functionality with responsive design, updated styles, and new images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,31 @@
-let current = 0;
-const slides = document.querySelectorAll('.hero-carousel img');
-const total = slides.length;
+const header = document.getElementById("myHeader");
+let scrollPosition = 0;
 
-setInterval(() => {
-    slides[current].classList.remove('active');
-    current = (current + 1) % total;
-    slides[current].classList.add('active');
-}, 5000);
+window.addEventListener("scroll", () => {
+    const currentScrollPosition = window.scrollY;
+
+    if (currentScrollPosition > 100) { // Ubah 100 menjadi posisi scroll yang Anda inginkan
+        header.classList.remove("transparent");
+        header.classList.add("solid");
+    } else {
+        header.classList.remove("solid");
+        header.classList.add("transparent");
+    }
+
+    scrollPosition = currentScrollPosition;
+});
+
+const navToggle = document.querySelector('.nav-toggle');
+const navList = document.querySelector('.nav-list');
+
+navToggle.addEventListener('click', () => {
+    navList.classList.toggle('show-nav');
+    navToggle.classList.toggle('nav-active');
+});
+
+document.addEventListener('click', (event) => {
+    if (!navList.contains(event.target) && !navToggle.contains(event.target)) {
+        navList.classList.remove('show-nav');
+        navToggle.classList.remove('nav-active');
+    }
+});
